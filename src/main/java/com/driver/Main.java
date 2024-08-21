65dc6f5cce7a34d2caf42caa78be9b47459fb604
Rohini Patil <rohine452001@gmail.com>
2024-08-21 07:16:35 +0530

    encapsulation assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        RWOnly obj = new RWOnly();
+
+        // Trying to set and access private variable directly
+        //obj.name = "John"; // This line will cause an error
+        //System.out.println(obj.name); // This line will also cause an error
+
+        // Errors:
+        // 'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("Shree");
+
+        System.out.println(obj.getName());
+    }
 }
